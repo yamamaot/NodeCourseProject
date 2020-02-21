c0c022800f8980234ca202d20c3e6049f18a325e
yamamaot <frozenstrawberryfree2@gmail.com>
2020-02-20 16:35:06 -0800

    added cart functionality and cart page buttons

--- index.js
+++ index.js
@@ -16,9 +16,15 @@ document.addEventListener("DOMContentLoaded", function(event){
     let product1 = new ProductObject("Speakers", 150);
     let product2 = new ProductObject("Remote control", 20);
 
+    //push each product to the product list
     productList.push(product0);
     productList.push(product1);
     productList.push(product2);
+
+    //fill cart with 0, as the cart is empty
+    for (var i = 0; i < productList.length; i++){
+        userCart.push(0);
+    }
 
 
     //console.log(productList);
@@ -33,20 +39,40 @@ document.addEventListener("DOMContentLoaded", function(event){
 
     displayRandomProduct();
 
+
+    function addToCart(pProduct){
+        for (var i = 0; i < productList.length; i++){
+            if (pProduct == productList[i]){
+                userCart[i] = userCart[i] + 1;
+            }
+        }
+        alert("Added one " + pProduct.ProductName + " to cart!");
+        console.log(userCart);
+    }
+
     //display name, picture, and price along with a button to buy
-    function displayProduct(pProduct, pHtmlName, pHtmlPrice, pImage){
+    function displayProduct(pProduct, pHtmlName, pHtmlPrice, pImage, pButton){
         document.getElementById(pHtmlName).innerHTML = pProduct.ProductName;
         document.getElementById(pHtmlPrice).innerHTML = "$" + pProduct.Price;  
         document.getElementById(pImage).src = "images/products/" + pProduct.ProductName + ".jpg";
-        document.getElementById("btnCart1").addEventListener("click", addToCart);
+        document.getElementById(pButton).addEventListener("click", function(){ addToCart(pProduct)});
     }
 
-    //test displayProduct method on page1
-    displayProduct(productList[0], "productName1", "productPrice1", "productImg1");
-    
-    function addToCart(){
-        //figure out this functionality later
+        //test displayProduct method on page1
+        displayProduct(productList[0], "productName1", "productPrice1", "productImg1", "btnCart1");
+
+    function displayCart(userCart, productList){
+        document.getElementById("stereoAmt").innerHTML = userCart[0];
+        document.getElementById("speakerAmt").innerHTML = userCart[1];
+        document.getElementById("remoteAmt").innerHTML = userCart[2];
     }
+
+    $(document).on("pagebeforeshow", "#page2", function(){
+        displayCart(userCart, productList)
+    });
+    
+    //button test
+    //document.getElementById("speakerMinus").addEventListener("click", function(){ alert("shits clicked yo")});
 
 });
 
