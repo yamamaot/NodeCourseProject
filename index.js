7ddfbfb5d0cdc2841ee82b0d0f7823dc005ec4ef
yamamaot <frozenstrawberryfree2@gmail.com>
2020-02-25 16:41:48 -0800

    more cart functionality added

--- index.js
+++ index.js
@@ -9,17 +9,22 @@ document.addEventListener("DOMContentLoaded", function(event){
     //however, user cart is simply integers indicating how many of each [indexed product] is in cart.
     //protections for going under 0 should be in place when at the cart page.
     let productList = [];
+    let priceList = [];
     let userCart = [];
     
 
     let product0 = new ProductObject("Stereo", 200);
     let product1 = new ProductObject("Speakers", 150);
     let product2 = new ProductObject("Remote control", 20);
+    
 
     //push each product to the product list
     productList.push(product0);
     productList.push(product1);
     productList.push(product2);
+    priceList.push(product0.Price);
+    priceList.push(product1.Price);
+    priceList.push(product2.Price);
 
     //fill cart with 0, as the cart is empty
     for (var i = 0; i < productList.length; i++){
@@ -40,7 +45,7 @@ document.addEventListener("DOMContentLoaded", function(event){
     displayRandomProduct();
 
 
-    function addToCart(pProduct){
+    function addToCartFirst(pProduct){
         for (var i = 0; i < productList.length; i++){
             if (pProduct == productList[i]){
                 userCart[i] = userCart[i] + 1;
@@ -55,24 +60,55 @@ document.addEventListener("DOMContentLoaded", function(event){
         document.getElementById(pHtmlName).innerHTML = pProduct.ProductName;
         document.getElementById(pHtmlPrice).innerHTML = "$" + pProduct.Price;  
         document.getElementById(pImage).src = "images/products/" + pProduct.ProductName + ".jpg";
-        document.getElementById(pButton).addEventListener("click", function(){ addToCart(pProduct)});
+        document.getElementById(pButton).addEventListener("click", function(){ addToCartFirst(pProduct)});
     }
 
         //test displayProduct method on page1
         displayProduct(productList[0], "productName1", "productPrice1", "productImg1", "btnCart1");
 
-    function displayCart(userCart, productList){
+    //inflexible display cart method
+    function displayCart(){
         document.getElementById("stereoAmt").innerHTML = userCart[0];
         document.getElementById("speakerAmt").innerHTML = userCart[1];
         document.getElementById("remoteAmt").innerHTML = userCart[2];
     }
 
+    //flexible add and subtract from cart methods
+    function addToCart(productInt){
+        userCart[productInt]++;
+        displayCart();
+    }
+
+    function subtractFromCart(productInt){
+        if (userCart[productInt] == 0){
+            alert("There are no items to remove!");
+        } else {
+            userCart[productInt]--;
+            displayCart();
+        }
+    }
+
+    function getTotal() {
+        let total = 0;
+        for(var i = 0; i < priceList.length; i++){
+            total += (priceList[i] * userCart[i]);
+        }
+        console.log(total);
+    }
+
+    document.getElementById("btnStereoMinus").addEventListener("click", function(){subtractFromCart(0)});
+    document.getElementById("btnStereoPlus").addEventListener("click", function(){addToCart(0)});
+    document.getElementById("btnSpeakerMinus").addEventListener("click", function(){subtractFromCart(1)});
+    document.getElementById("btnSpeakerPlus").addEventListener("click", function(){addToCart(1)});
+    document.getElementById("btnRemoteMinus").addEventListener("click", function(){subtractFromCart(2)});
+    document.getElementById("btnRemotePlus").addEventListener("click", function(){addToCart(2)});
+
     $(document).on("pagebeforeshow", "#page2", function(){
-        displayCart(userCart, productList)
+        displayCart();
     });
-    
-    //button test
-    //document.getElementById("speakerMinus").addEventListener("click", function(){ alert("shits clicked yo")});
+    $(document).on("pagebeforeshow", "#page3", function(){
+        getTotal();
+    });
 
 });
 
